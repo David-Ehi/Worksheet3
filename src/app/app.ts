03dfc5c173b53c5c3e7073666f3212d688d3ad81
David EhiagwinaS00 <you@example.com>
2026-02-04 10:33:41 +0000

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,36 @@
+import { tmplAstVisitAll } from '@angular/compiler';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Week3 - Fav Movies');
+
+    protected readonly movies = signal<string[]>([
+    'The Dark Knight',
+    'Inception',
+    'Avengers: Endgame',
+    'Interstellar',
+    'Titanic'
+  ]);
+
+  protected favourites = signal<string[]>([
+  ])
+
+  protected addFavourite(fav:string) {
+    this.favourites.update(current => [...current, fav]);
+  }
+ 
+  protected removeFavourites(index:number){
+    this.favourites.update(favourites =>
+      favourites.filter((_, i) => i !== index)
+    )
+  }
+
+}
+
